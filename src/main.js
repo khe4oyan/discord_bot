2b37d658c2e5e0290253c5a78b214b33ca5d10ed
khc <khe4oyan@gmail.com>
2024-10-15 23:23:18 +0400

    ,

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences,
   ],
 });
 
@@ -33,5 +34,26 @@ client.on(Events.InteractionCreate, async (interaction) => {
 client.on(Events.GuildDelete, async (guildId) => {
   EventGuildDelete(guildId);
 });
+
+// для проверки статуса
+// client.on(Events.PresenceUpdate, (oldPresence, newPresence) => {
+//   // Проверяем, если старое присутствие существует
+//   if (oldPresence) {
+//       // Если статус изменился
+//       if (oldPresence.status !== newPresence.status) {
+//           // Логируем изменение статуса
+//           console.log(`${newPresence.user.tag} изменил статус: ${oldPresence.status} -> ${newPresence.status}`);
+          
+//           // Проверяем, если пользователь стал онлайн
+//           if (newPresence.status === 'online') {
+//               console.log(`${newPresence.user.tag} теперь в сети!`);
+//           }
+//           // Проверяем, если пользователь стал оффлайн
+//           if (newPresence.status === 'offline') {
+//               console.log(`${newPresence.user.tag} вышел из сети.`);
+//           }
+//       }
+//   }
+// });
 
 client.login(process.env.DISCORD_TOKEN);
