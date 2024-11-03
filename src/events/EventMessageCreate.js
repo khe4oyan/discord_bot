165ff25f4c9ae63b0b0655f8368e7b96126b2411
khc <khe4oyan@gmail.com>
2024-11-04 01:27:12 +0400

    .

--- src/events/EventMessageCreate.js
+++ src/events/EventMessageCreate.js
@@ -1,11 +1,20 @@
 const UserData = require('../classes/UserData.js');
 
+const processingUsers = new Set();
+
 module.exports = async function EventMessageCreate(message, client) {
-  if (message.content === "res") {
-    client.destroy();
-    client.login(process.env.DISCORD_TOKEN);
+  const userId = message.author.id;
+  if (processingUsers.has(userId)) { return; }
+
+  processingUsers.add(userId);
+
+  if (userId === process.env.OWNER_ID) {
+    if (message.content.includes('!дай денег')) {
+      message.reply("Как я это сделаю? Гений..");
+    }
   }
 
   if (message.author.bot) return;
   new UserData(message.author, message.guildId).incrementBalance();
+  processingUsers.delete(userId);
 };
