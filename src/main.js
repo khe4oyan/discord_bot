686543c0e322207a4178e76f612131da15749e41
khe4oyan <khe4oyan@gmail.com>
2026-01-29 15:08:22 +0400

    [feat] remove web version

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ const {
   Collection,
   Events,
 } = require("discord.js");
-const express = require("express");
-const app = express();
-const path = require("path");
 
 // events
 const EventInteractionCreate = require("./events/EventInteractionCreate.js");
@@ -18,119 +15,52 @@ const EventGuildDelete = require("./events/EventGuildDelete.js");
 let client = null;
 
 // Создаем новый клиент
-function onBot() {
-  client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-      GatewayIntentBits.GuildPresences,
-    ],
-  });
-
-  client.commands = new Collection();
-
-  client.once(Events.ClientReady, async () => {
-    EventClientReady(client);
-  });
-
-  client.on(Events.MessageCreate, async (message) => {
-    EventMessageCreate(message, client);
-  });
-
-  client.on(Events.InteractionCreate, async (interaction) => {
-    EventInteractionCreate(interaction, client);
-  });
-
-  client.on(Events.GuildDelete, async (guildId) => {
-    EventGuildDelete(guildId);
-  });
-
-  // для проверки статуса
-  // client.on(Events.PresenceUpdate, (oldPresence, newPresence) => {
-  //   // Проверяем, если старое присутствие существует
-  //   if (oldPresence) {
-  //       // Если статус изменился
-  //       if (oldPresence.status !== newPresence.status) {
-  //           // Логируем изменение статуса
-  //           console.log(`${newPresence.user.tag} изменил статус: ${oldPresence.status} -> ${newPresence.status}`);
-
-  //           // Проверяем, если пользователь стал онлайн
-  //           if (newPresence.status === 'online') {
-  //               console.log(`${newPresence.user.tag} теперь в сети!`);
-  //           }
-  //           // Проверяем, если пользователь стал оффлайн
-  //           if (newPresence.status === 'offline') {
-  //               console.log(`${newPresence.user.tag} вышел из сети.`);
-  //           }
-  //       }
-  //   }
-  // });
-
-  client.login(process.env.DISCORD_TOKEN);
-}
-
-function offBot() {
-  client.destroy();
-  client = null;
-}
-
-const port = process.env.SERVER_PORT;
-
-app.use(express.static(path.join(__dirname, "../html")));
-app.use(express.json());
-
-app.get("/", (_, res) => {
-  res.sendFile(path.join(__dirname, "../html/index.html"));
+client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences,
+  ],
 });
 
-app.get("/status", (_, res) => {
-  if (client) {
-    res.send({ status: 200, message: "Bot online" });
-  } else {
-    res.send({ status: 201, message: "Bot offline" });
-  }
+client.commands = new Collection();
+
+client.once(Events.ClientReady, async () => {
+  EventClientReady(client);
 });
 
-app.post("/on", (req, res) => {
-  if (!passValidation(req.body.password, res)) {
-    return;
-  }
-
-  if (client) {
-    res.send({ status: 404, message: "Bot already online" });
-    return;
-  }
-
-  onBot();
-  res.send({ status: 200, message: "Bot now is online" });
+client.on(Events.MessageCreate, async (message) => {
+  EventMessageCreate(message, client);
 });
 
-app.post("/off", (req, res) => {
-  if (!passValidation(req.body.password, res)) {
-    return;
-  }
-
-  if (!client) {
-    res.send({ status: 404, message: "Bot already is offline" });
-    return;
-  }
-
-  offBot();
-  res.send({ status: 200, message: "Bot now is offline" });
+client.on(Events.InteractionCreate, async (interaction) => {
+  EventInteractionCreate(interaction, client);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+client.on(Events.GuildDelete, async (guildId) => {
+  EventGuildDelete(guildId);
 });
 
-function passValidation(password, res) {
-  if (password !== process.env.PASSWORD) {
-    res.send({ status: 444, message: "Invalid password" });
-    return false;
-  }
+// для проверки статуса
+// client.on(Events.PresenceUpdate, (oldPresence, newPresence) => {
+//   // Проверяем, если старое присутствие существует
+//   if (oldPresence) {
+//       // Если статус изменился
+//       if (oldPresence.status !== newPresence.status) {
+//           // Логируем изменение статуса
+//           console.log(`${newPresence.user.tag} изменил статус: ${oldPresence.status} -> ${newPresence.status}`);
 
-  return true;
-}
+//           // Проверяем, если пользователь стал онлайн
+//           if (newPresence.status === 'online') {
+//               console.log(`${newPresence.user.tag} теперь в сети!`);
+//           }
+//           // Проверяем, если пользователь стал оффлайн
+//           if (newPresence.status === 'offline') {
+//               console.log(`${newPresence.user.tag} вышел из сети.`);
+//           }
+//       }
+//   }
+// });
 
-onBot();
+client.login(process.env.DISCORD_TOKEN);
