29e87f58a004e532b63cca3408974be86f0b84e3
khe4oyan <khe4oyan@gmail.com>
2026-01-30 15:15:24 +0400

    [optimize] move data change from FilManager to MySQL

--- src/events/EventMessageCreate.js
+++ src/events/EventMessageCreate.js
@@ -1,15 +1,24 @@
+// repo
+const UserRepo = require("../repository/UserRepo.js");
+
 const processingUsers = new Set();
 
 module.exports = async function EventMessageCreate(message) {
+  console.log("[EVENT]: Message");
+  
   const userId = message.author.id;
+  if (message.content[0] === "/") return;
 
   if (processingUsers.has(userId)) { return; }
   processingUsers.add(userId);
 
   if (message.author.bot) return;
-  
-  // TODO: increment balance
-  console.log(message.author.id);
+
+  try {
+    await UserRepo.incrementBalance(message.author);
+  } catch (error) {
+    console.log("[CRITICAL ERROR]", error.message);
+  }
   
   processingUsers.delete(userId);
 };
