32b929a36ddf8c13173580733cfebfcedd936b44
khe4oyan <khe4oyan@gmail.com>
2026-01-30 15:15:57 +0400

    [feat] add mysql

---
+++ src/repository/UserRepo.js
@@ -0,0 +1,19 @@
+const pool = require("../config/pool.js");
+
+class UserRepo {
+  static async #createUserIfNotExists(user) {
+    await pool.execute(`
+      INSERT IGNORE 
+      INTO users (discord_id, username, global_name, balance, items)
+      VALUES (?, ?, ?, ?, ?)`,
+      [user.id, user.username, user.globalName, 0, "[]"]
+    );
+  }
+
+  static async incrementBalance(user) {
+    await UserRepo.#createUserIfNotExists(user);
+    await pool.execute(`UPDATE users SET balance = balance + 1 WHERE discord_id = ?`, [user.id]);
+  }
+};
+
+module.exports = UserRepo;
